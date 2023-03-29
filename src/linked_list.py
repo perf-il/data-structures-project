31e31f5b43851b3149c34866343189ec713442cd
Ilia Perfilov <ind.climb@gmail.com>
2023-03-29 15:23:45 +0300

    linked_list.py was modified - new methods insert_beginning and insert_at_end added

--- src/linked_list.py
+++ src/linked_list.py
@@ -1,18 +1,39 @@
 class Node:
     """Класс для узла односвязного списка"""
-    pass
+    def __init__(self, data, next_node=None):
+        self.data = data
+        self.next_node = next_node
 
 
 class LinkedList:
     """Класс для односвязного списка"""
+    def __init__(self):
+        self.head: Node | None = None
+        self.tail: Node | None = None
+        self.size: int = 0
 
     def insert_beginning(self, data: dict) -> None:
         """Принимает данные (словарь) и добавляет узел с этими данными в начало связанного списка"""
-        pass
+        head = self.head
+        if head is None:
+            self.head = self.tail = Node(data)
+        else:
+            self.head = Node(data, head)
+        self.size += 1
 
     def insert_at_end(self, data: dict) -> None:
         """Принимает данные (словарь) и добавляет узел с этими данными в конец связанного списка"""
-        pass
+        tail = self.tail
+        if tail is None:
+            self.tail = self.head = Node(data)
+        else:
+            cur = self.head
+            while cur.next_node is not None:
+                cur = cur.next_node
+            cur.next_node = tail = Node(data)
+            self.tail.next_node = tail
+            self.tail = tail
+        self.size += 1
 
     def __str__(self) -> str:
         """Вывод данных односвязного списка в строковом представлении"""
@@ -26,4 +47,5 @@ class LinkedList:
             node = node.next_node
 
         ll_string += ' None'
-        return ll_string
+        return ll_string.strip()
+
